2e797ac345b3e547cdf6479b92c1f7729c2c16d5
Lydia <lydia.h.kang@gmail.com>
2022-08-17 07:39:17 -0700

    Initial Commit

---
+++ EsteeLauder(1)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EsteeLauder(1)
+//
+//  Created by Lydia Kang on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
